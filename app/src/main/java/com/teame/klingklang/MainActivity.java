6150596d07ee47673c7f971e8c2ce962cd341fd0
Souhail Arab <sa247031@fh-muenster.de>
2022-10-24 15:26:59 +0200

    force landscape modus and hide grey stripe on SwipeUp Bar/Navigation Bar

--- app/src/main/java/com/teame/klingklang/MainActivity.java
+++ app/src/main/java/com/teame/klingklang/MainActivity.java
@@ -3,21 +3,49 @@ package com.teame.klingklang;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        //Navigation/swipeUp bar nicht anzeigen
+        hideNavigationAndSwipeUpBar();
+
+        //Setze die Orientierung au landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        setContentView(R.layout.activity_main);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 
     public void openMenu(View view) {
         mDrawerLayout.openDrawer(Gravity.LEFT);
+    }
+
+    private void hideNavigationAndSwipeUpBar() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
+    /*
+    nach dem Verlassen der App und Zurückkehren bleiben die Einstellungen erhalten
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideNavigationAndSwipeUpBar();
     }
 }
