27bb47df3808e76522c9bec0e2e5d636e4893e94
Alexander Huelsmann <ah070956@fh-muenster.de>
2022-11-06 19:24:57 +0100

    Basic MainMenu

--- app/src/main/java/de/gruppe/e/klingklang/MainMenu.java
+++ app/src/main/java/de/gruppe/e/klingklang/MainMenu.java
@@ -1,38 +1,21 @@
 package de.gruppe.e.klingklang;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.pm.ActivityInfo;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.LayerDrawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
-import com.google.android.material.slider.Slider;
-
-import java.util.Locale;
-import java.util.Objects;
 
 
 public class MainMenu extends BottomSheetDialogFragment {
@@ -51,10 +34,6 @@ public class MainMenu extends BottomSheetDialogFragment {
     @Override public Dialog onCreateDialog(Bundle savedInstanceState) {
         Dialog dialog = super.onCreateDialog(savedInstanceState);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
-            setWhiteNavigationBar(dialog);
-        }
-
         dialog.setOnShowListener(dialogInterface -> {
             BottomSheetDialog bottomSheetDialog = (BottomSheetDialog) dialogInterface;
             setupFullHeight(bottomSheetDialog);
@@ -62,28 +41,6 @@ public class MainMenu extends BottomSheetDialogFragment {
         return  dialog;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    private void setWhiteNavigationBar(@NonNull Dialog dialog) {
-        Window window = dialog.getWindow();
-        if (window != null) {
-            DisplayMetrics metrics = new DisplayMetrics();
-            window.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-            GradientDrawable dimDrawable = new GradientDrawable();
-            // ...customize your dim effect here
-
-            GradientDrawable navigationBarDrawable = new GradientDrawable();
-            navigationBarDrawable.setShape(GradientDrawable.RECTANGLE);
-            navigationBarDrawable.setColor(Color.WHITE);
-
-            Drawable[] layers = {dimDrawable, navigationBarDrawable};
-
-            LayerDrawable windowBackground = new LayerDrawable(layers);
-            windowBackground.setLayerInsetTop(1, metrics.heightPixels);
-
-            window.setBackgroundDrawable(windowBackground);
-        }
-    }
 
     /**
      * Sets the Behaviour of the BottomSheetDialog so it expands over the whole screen
@@ -131,7 +88,7 @@ public class MainMenu extends BottomSheetDialogFragment {
     private void createMainMenu() {
         ImageButton settingsButton = view.findViewById(R.id.Einstellungen);
         ImageButton recordingsButton = view.findViewById(R.id.Aufnahmen);
-        ImageButton bpmButton = view.findViewById(R.id.BPM);
+        ImageButton taktButton = view.findViewById(R.id.Takt);
         ImageButton exitButton = view.findViewById(R.id.exitButton);
 
         exitButton.setOnClickListener(view -> dismiss());
