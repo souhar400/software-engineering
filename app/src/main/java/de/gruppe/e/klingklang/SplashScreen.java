2ffa6eb0077b06347418fa5d4fc55d6da9db6c71
Johny <fj135730@fh-muenster.de>
2022-11-07 17:31:36 +0100

    splash_screen___5.xml eingefügt sowie Start Activity auf SplashScreen.java im AndroidManifest.xml geändert

---
+++ app/src/main/java/de/gruppe/e/klingklang/SplashScreen.java
@@ -0,0 +1,26 @@
+package de.gruppe.e.klingklang;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
+    }
+}
