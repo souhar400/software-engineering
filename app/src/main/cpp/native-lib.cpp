fc983b4872924444187c247bd33ba33ba8baa9dd
Eren <ErenOzkacar@icloud.com>
2022-11-08 00:59:26 +0100

    Change structure of native-lib

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -3,49 +3,72 @@
 #include <fluidsynth.h>
 #include <unistd.h>
 
+bool initialized = false;
+fluid_settings_t *settings;
+fluid_synth_t *synth = NULL;
+fluid_audio_driver_t *adriver = NULL;
+
+void initialize();
+
+void cleanup();
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_de_gruppe_e_klingklang_MainActivity_playFluidSynthSound(JNIEnv *env, jobject thiz,
                                                              jstring soundfont_path,
                                                              jint sound_length) {
-    fluid_settings_t *settings;
-    fluid_synth_t *synth = NULL;
-    fluid_audio_driver_t *adriver = NULL;
+    initialize();
+
     const char *soundfontPath = env->GetStringUTFChars(soundfont_path, nullptr);
-
-    // Create the settings object with default values
-    settings = new_fluid_settings();
-    if (settings == NULL) {
-        fprintf(stderr, "Failed to create the settings\n");
-        goto cleanup;
-    }
-
-    // Create the synthesizer
-    synth = new_fluid_synth(settings);
-    if (synth == NULL) {
-        fprintf(stderr, "Failed to create the synthesizer\n");
-        goto cleanup;
-    }
-
-    // Create the audio driver to play the synthesizer
-    adriver = new_fluid_audio_driver(settings, synth);
-    if (adriver == NULL) {
-        fprintf(stderr, "Failed to create the audio driver\n");
-        goto cleanup;
-    }
-
     // Load the soundfont
     if (fluid_synth_sfload(synth, soundfontPath, 1) == -1) {
         fprintf(stderr, "Failed to load soundfont\n");
-        goto cleanup;
+        cleanup();
     }
 
     // Play the sound
     fluid_synth_noteon(synth, 0, 62, 127);
     sleep(sound_length);
     fluid_synth_noteoff(synth, 0, 62);
+}
 
-    cleanup:
+extern "C"
+JNIEXPORT void JNICALL
+Java_de_gruppe_e_klingklang_MainActivity_playLoopedSynthSound(JNIEnv *env, jobject thiz,
+                                                              jstring soundfont_path) {
+    initialize();
+}
+
+void initialize() {
+    if (!initialized) {
+        // Create the settings object with default values
+        settings = new_fluid_settings();
+        if (settings == NULL) {
+            fprintf(stderr, "Failed to create the settings\n");
+            cleanup();
+            return;
+        }
+
+        // Create the synthesizer
+        synth = new_fluid_synth(settings);
+        if (synth == NULL) {
+            fprintf(stderr, "Failed to create the synthesizer\n");
+            cleanup();
+            return;
+        }
+
+        // Create the audio driver to play the synthesizer
+        adriver = new_fluid_audio_driver(settings, synth);
+        if (adriver == NULL) {
+            fprintf(stderr, "Failed to create the audio driver\n");
+            cleanup();
+            return;
+        }
+        initialized = true;
+    }
+}
+
+void cleanup() {
     if (adriver) {
         delete_fluid_audio_driver(adriver);
     }
@@ -57,4 +80,5 @@ Java_de_gruppe_e_klingklang_MainActivity_playFluidSynthSound(JNIEnv *env, jobjec
     if (settings) {
         delete_fluid_settings(settings);
     }
+    initialized = false;
 }
